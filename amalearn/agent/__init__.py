c3bb395926c2879533f1afab7face22c3919b237
zhivar <zhivarsourati@gmail.com>
2020-11-01 16:16:24 +0330

    Add uni agent and also the environment to test it

--- amalearn/agent/__init__.py
+++ amalearn/agent/__init__.py
@@ -2,3 +2,4 @@ from amalearn.agent.agent_base import AgentBase
 from amalearn.agent.random_bandit_agent import RandomBanditAgent
 from amalearn.agent.ten_armed_bandit_agent import TenArmedBanditAgent 
 from amalearn.agent.social_agent import SocialAgent
+from amalearn.agent.uni_agent import UniAgent
