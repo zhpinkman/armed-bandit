09606e14f54abdb01ce3dc4435f0b2ad339255a0
zhivar <zhivarsourati@gmail.com>
2020-11-11 01:27:12 +0330

    Add gradient method policy for part 3 and its results showing rate of using the optimum action

--- amalearn/agent/net_agent.py
+++ amalearn/agent/net_agent.py
@@ -8,32 +8,57 @@ class NetAgent(AgentBase):
         self.qValues = list()
         self.counts = list()
         self.observation = list()
+        self.h = list()
         self.eps = .5
+        self.average_reward = 0
+        self.lr = .1
 
     def setup(self):
         actions_n = self.environment.available_actions()
         self.qValues = [100.0 for i in range(actions_n)]
         self.counts = [0 for i in range(actions_n)]
         self.observation = [list() for i in range(actions_n)]
+        self.h = [0 for i in range(actions_n)]
 
 
     def pick_arm(self):
         available_actions = self.environment.available_actions()
-        p = np.random.random() 
-        if p < self.eps:
-            chosen_arm_index = np.random.choice(available_actions)
+        if self.policy == 'eGreedy':
+            p = np.random.random() 
+            if p < self.eps:
+                chosen_arm_index = np.random.choice(available_actions)
+            else:
+                chosen_arm_index = np.argmin(self.qValues)
+            return chosen_arm_index
         else:
-            chosen_arm_index = np.argmin(self.qValues)
-        return chosen_arm_index
+            probs = [np.exp(i)/sum(np.exp(self.h)) for i in self.h]
+            chosen_arm_index = np.random.choice(np.arange(available_actions), p=probs)
+            return chosen_arm_index
+
         
         
         
     def update_params(self, chosen_arm_index, reward):
-        self.counts[chosen_arm_index] += 1
-        n = self.counts[chosen_arm_index]
-        value = self.qValues[chosen_arm_index]
-        new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
-        self.qValues[chosen_arm_index] = new_value
+        if self.policy == 'eGreedy':
+            self.counts[chosen_arm_index] += 1
+            n = self.counts[chosen_arm_index]
+            value = self.qValues[chosen_arm_index]
+            new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
+            self.qValues[chosen_arm_index] = new_value
+        else: 
+            reward *= -1
+            probs = [np.exp(i)/sum(np.exp(self.h)) for i in self.h]
+            # print(probs)
+            self.counts[chosen_arm_index] += 1
+            n = sum(self.counts)
+            for i in range(len(self.h)):
+                if i == chosen_arm_index:
+                    self.h[i] += self.lr * (reward - self.average_reward) * (1 - probs[i])
+                else:
+                    self.h[i] -= self.lr * (reward - self.average_reward) * probs[i]
+            self.average_reward = (self.average_reward * (n-1) + reward ) / n
+                
+        
 
 
         
@@ -44,6 +69,10 @@ class NetAgent(AgentBase):
         observation, reward, done, info = self.environment.step(chosen_arm_index)
         self.observation[chosen_arm_index].append(reward)
         self.update_params(chosen_arm_index, reward)
+        # print(self.h)
         if sum(self.counts) % 10 == 0:
             self.eps -= 0.0005 
-        return self.counts, self.qValues
+        if self.policy == 'eGreedy':
+            return self.counts, self.qValues
+        else: 
+            return self.counts, self.h
--- example/net_bandit.py
+++ example/net_bandit.py
@@ -16,7 +16,7 @@ def plot_qValues(plt, qValues):
     means = np.mean(qValues, axis=0)
     plt.plot(means)
     plt.grid()
-    plt.xticks(np.arange(len(means)))
+    plt.xticks(np.arange(len(means), step=2))
     plt.title('qValues corresponding to every arm which is a path thorough network')
     plt.xlabel('Path index')
     plt.ylabel('qValue')
@@ -25,25 +25,18 @@ def plot_AR(plt, AR):
     ARR_array = np.array(AR)
     ARR_means = np.mean(ARR_array, axis=0)
     max_ARR_means = np.max(ARR_means, axis=0)
+    max_index = []
+    for i in ARR_means:
+        max_index.append(i[-1])
     plt.plot(100 * max_ARR_means)
-    plt.title('Average rate of using optimal action')
+    legend_text = 'path index: ' + str(np.argmax(max_index))
+    plt.legend([legend_text])
+    plt.title('Average rate of using optimal action with lr = 0.01')
     plt.xlabel('Trials')
     plt.ylabel('AR')
     plt.grid(axis='y')
     plt.annotate(str(np.round(100*max_ARR_means[-1])) + '%', (len(max_ARR_means), 100*max_ARR_means[-1]))
 
-
-def perform_one_epoch(fig, number_of_arms, value, index):
-    rewards = [UniReward() for i in range(number_of_arms)]
-    env = UniEnv(rewards, 100000, '1')
-    agent = UniAgent('1', env, 'eGreedy')
-    agent.setup()
-    results = [list() for i in range(number_of_arms)]
-    for step in range(100000):
-        counts, qValues = agent.take_action()
-        for i in range(number_of_arms):
-            results[i].append(counts[i] / sum(counts))
-    return results, qValues
 
 
 c2_list = [1, 2, 3]
@@ -54,15 +47,18 @@ c4_list = [9, 10, 11]
 rewards = [NetReward(c1, c2, c3) for c1, c2, c3 in itertools.product(c2_list, c3_list, c4_list)]
 env = NetEnv(rewards, 10000, '1')
 agent = NetAgent('1', env, 'eGreedy')
+agent2 = NetAgent('2', env, 'gradient')
 agent.setup()
+agent2.setup()
 
 agg_results = []
 agg_qValues = []
 
-for i in range(20):
+for i in range(5):
+    print('epoch: ', i + 1)
     results = [list() for i in range(number_of_arms)]
     for step in range(10000):
-        counts, qValues = agent.take_action()
+        counts, qValues = agent2.take_action()
         for i in range(number_of_arms):
             results[i].append(counts[i] / sum(counts))
     agg_results.append(results)
@@ -70,7 +66,9 @@ for i in range(20):
 
 
 
-
+plt.figure()
+plt.subplot(121)
 plot_AR(plt, agg_results)
-# plot_qValues(plt, agg_qValues)
+plt.subplot(122)
+plot_qValues(plt, agg_qValues)
 plt.show()
